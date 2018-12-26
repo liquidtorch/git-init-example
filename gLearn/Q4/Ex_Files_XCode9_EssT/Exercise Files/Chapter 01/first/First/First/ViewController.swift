17761e5e0d52dff96a2be4f7e122c3e82bf5a21b
Andy Zalit <andyzalit@gmail.com>
2018-12-26 11:53:07 -0700

    Added print statement

---
+++ gLearn/Q4/Ex_Files_XCode9_EssT/Exercise Files/Chapter 01/first/First/First/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  First
+//
+//  Created by Andy Zalit on 12/26/18.
+//  Copyright © 2018 Andy Zalit. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		// Do any additional setup after loading the view.
+	}
+
+	override var representedObject: Any? {
+		didSet {
+		// Update the view, if already loaded.
+		}
+	}
+
+
+}
+
